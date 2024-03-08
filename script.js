08f1ad8235bfb9c8df5cd235bf3ac154e5658d71
rajimicheal <147432405+rajimicheal@users.noreply.github.com>
2024-03-08 03:21:31 -0500

    init

---
+++ script.js
@@ -0,0 +1,43 @@
+playerchat = document.getElementById("chat_1")
+let firstchat = prompt("What is your name")
+playerchat.textContent = firstchat;
+
+playerchat_2 = document.getElementById("chat_2")
+let secondchat = prompt("What is your favorite Food")
+playerchat_2.textContent = secondchat;
+
+playerchat_3 = document.getElementById("chat_3")
+let thirdchat = prompt("What is your favorite Food")
+playerchat_3.textContent = thirdchat;
+
+playerchat_4 = document.getElementById("chat_4")
+let fourthchat = prompt("What is your favorite Food")
+playerchat_4.textContent = fourthchat;
+
+playerchat_5 = document.getElementById("chat_5")
+let fifthchat = prompt("What is your favorite Food")
+playerchat_5.textContent = fifthchat;
+
+playerchat_6 = document.getElementById("chat_6")
+let sixthchat = prompt("What is your favorite Food")
+playerchat_6.textContent = sixthchat;
+
+playerchat_7 = document.getElementById("chat_7")
+let seventchat = prompt("What is your favorite Food")
+playerchat_7.textContent = seventchat;
+
+playerchat_8 = document.getElementById("chat_8")
+let Eightchat = prompt("What is your favorite Food")
+playerchat_8.textContent = Eightchat;
+
+playerchat_9 = document.getElementById("chat_9")
+let ninthchat = prompt("What is your favorite Food")
+playerchat_9.textContent = ninthchat;
+
+playerchat_10 = document.getElementById("chat_10")
+let tenthchat = prompt("What is your favorite Food")
+playerchat_10.textContent =tenthchat ;
+
+playerchat_11 = document.getElementById("chat_11")
+let chat = prompt("What is your favorite Food")
+playerchat_11.textContent = chat;
